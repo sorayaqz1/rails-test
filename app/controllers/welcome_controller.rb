6e9e0bb38758506fc77742826978a97103bc24b8
Qing Zhang <qzhang@qings-air.lan>
2018-04-19 11:43:04 -0500

    retry commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def About
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  #
+  root 'welcome#index'
+
+  get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
